98f4ae92657e2d662a85edd21ee2b58ebf1bd605
Nathan Cairns <nathancairns777@gmail.com>
2018-09-15 15:22:21 +1200

    Write inSameTree/2 rule

--- src/family.pl
+++ src/family.pl
@@ -1,16 +1,27 @@
 % Contains logic definitions for the family predicates motherOf/2, sisterOf/2 and inSameTree/2.
 % Contains the prolog implementations of these definitions.
-% Assumes we have the fact predicates childOf(child, parent), male(person), female(person)
 %
 % AUTHOR: NATHAN CAIRNS
 
 % Rules %
 
 % motherOf(mother, person) - decides whether a person is a mother of another person
+% Assumes we have the fact predicates childOf(child, parent), male(person), female(person)
+% Mother is the mother of Person if Person is a child of Mother and Mother is female.
 motherOf(Mother, Person):- childOf(Person, Mother), female(Mother).
  
 % sisterOf(sister, person) - decides whether a person is a sister of another person
+% Assumes we have the fact predicates childOf(child, parent), male(person), female(person)
+% Sister is the sister of Person is Sister is female and Person and Sister are the child of the same Parent.
 sisterOf(Sister, Person) :-  female(Sister), childOf(Person, Parent), childOf(Sister, Parent).
  
-% inSameTree(node1, node2) - decides wheter node1 and node2 are in the same tree
-inSameTree(node1, node2) :-
+% inSameTree(node1, node2) - decides whether node1 and node2 are in the same tree
+% Assumes we have the fact predicates of parentOf(parent, child)
+% Two nodes node1 and node2 are in the same tree if they share a common ancestor.
+inSameTree(Node1, Node2) :- ancestorOf(Ancestor, Node1), ancestorOf(Ancestor, Node2).
+
+% ancestorOf(Ancestor, Descendent) - decides whether Ancestor is an ancestor of Descendent.
+% Assumes we have the fact predicates of parentOf(parent, child)
+ancestorOf(Ancestor, Descendent) :- parentOf(Ancestor, Descendent).
+ancestorOf(Ancestor, Descendent) :- parentOf(Ancestor, Child), ancestorOf(Child, Descendent).
+
--- src/familyTest.pl
+++ src/familyTest.pl
@@ -9,7 +9,6 @@
 % Facts %
 
 % childOF/2
-
 childOf(bob, steve). % should fail motherof
 childOf(bob, elizabeth). % should pass motherof
 childOf(bob, steph). % should fail motherof
@@ -18,12 +17,28 @@ childOf(rob, steve). % rob / bob should fail sisterof
 childOf(eden, steve). % eden / bob should succeed sisterof
 
 % Female/1
-
 female(elizabeth).
 female(eden).
 
 % Male/1
-
 male(steve).
 male(rob).
 male(bob).
+
+% Test facts for inSameTree/2
+
+% calling inSameTree(node1, node2) should return true
+parentOf(node3, node1).
+parentOf(node3, node2).
+
+% calling inSameTree(nodeA1, nodeA2) should return false
+parentOf(nodeA3, nodeA1).
+parentOf(nodeA4, nodeA2).
+
+% calling inSameTree(nodeB1, nodeB2) should return true
+parentOf(nodeB2, nodeB1).
+parentOf(nodeB3, nodeB2).
+parentOf(nodeB4, nodeB3).
+
+parentOf(nodeB6, nodeB5).
+parentOf(nodeB7, nodeB4).
