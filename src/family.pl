00501c25bb599eb008345894277a57410b2617f0
Nathan Cairns <nathancairns777@gmail.com>
2018-09-15 13:05:48 +1200

    File stubs

---
+++ src/family.pl
@@ -0,0 +1,10 @@
+/* Assume we have the fact predicates ChildOf(child, parent), Male(person), Female(person) */
+
+ /* motherOf/2 def */
+ MotherOf(mother, person)
+ 
+ /* sisterOf/2 def */
+ SisterOf(sister, person)
+ 
+ /* inSameTree/2 def */
+ inSameTree(node1, node2)
